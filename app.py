e7c6c281357662022d9332cecaf3fed129cf2655
merkato <tomasz.merkato@gmail.com>
2017-06-17 07:41:55 +0200

    Porządkowanie zakładki terminy. Uszablonowienie obsługi 404

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json, redirect, url_for, request
-from peewee import * 
+from peewee import *
 from models import *
 
 app = Flask(__name__)
@@ -9,41 +9,8 @@ app.config.from_pyfile('config.py', silent=True)
 def index():
     return render_template("index.html")
 
-def login_required(fn):
-    @functools.wraps(fn)
-    def inner(*args, **kwargs):
-        if session.get('logged_in'):
-            return fn(*args, **kwargs)
-        return redirect(url_for('login', next=request.path))
-    return inner
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    next_url = request.args.get('next') or request.form.get('next')
-    if request.method == 'POST' and request.form.get('password'):
-        password = request.form.get('password')
-        if password == app.config['ADMIN_PASSWORD']:
-            session['logged_in'] = True
-            session.permanent = True  # Use cookie to store session.
-            flash('You are now logged in.', 'success')
-            return redirect(next_url or url_for('index'))
-        else:
-            flash('Incorrect password.', 'danger')
-    return render_template('login.html', next_url=next_url)
-
-@app.route('/logout/', methods=['GET', 'POST'])
-def logout():
-    if request.method == 'POST':
-        session.clear()
-        return redirect(url_for('login'))
-    return render_template('logout.html')
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route("/dodaj_wolne", methods=["GET","POST"])
-def addholiday():
+@app.route("/wolne", methods=["GET","POST"])
+def holiday():
 	if request.method == 'POST':
 		termin = request.form.get('inputTermin')
 		opis = request.form.get('inputOpis')
@@ -51,15 +18,41 @@ def addholiday():
 		q = holidays.insert(termin = termin, opis = opis, wariant = wariant)
 		q.execute()
 		return redirect(url_for('holiday'))
-	return render_template("holiday.html")
+	return render_template("holiday.html",wolne=list(holidays.select().order_by(holidays.termin.asc())))
 
-@app.route("/wolne")
-def holiday():
-	return render_template("holiday_list.html",wolne=list(holidays.select()))
+@app.route("/pociagi", methods=["GET","POST"])
+def trains():
+	if request.method == 'POST':
+		termin = request.form.get('inputTermin')
+		opis = request.form.get('inputOpis')
+		wariant = request.form.get('inputWariant')
+		q = holidays.insert(termin = termin, opis = opis, wariant = wariant)
+		q.execute()
+		return redirect(url_for('holiday'))
+	return render_template("holiday.html",wolne=list(holidays.select()))
+
+@app.route("/sluzby", methods=["GET","POST"])
+def shifts():
+	if request.method == 'POST':
+		termin = request.form.get('inputTermin')
+		opis = request.form.get('inputOpis')
+		wariant = request.form.get('inputWariant')
+		q = holidays.insert(termin = termin, opis = opis, wariant = wariant)
+		q.execute()
+		return redirect(url_for('holiday'))
+	return render_template("holiday.html",wolne=list(holidays.select()))
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('error.html'), 404
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=49666)
